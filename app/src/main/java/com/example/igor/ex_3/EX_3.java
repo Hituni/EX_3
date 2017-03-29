b2f8b659148c03cdd8940ec792dc4f8cf9141e34
master <box.fomn-30@ya.ru>
2017-03-29 13:57:35 +0300

    android day 2

--- app/src/main/java/com/example/igor/ex_3/EX_3.java
+++ app/src/main/java/com/example/igor/ex_3/EX_3.java
@@ -4,12 +4,12 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.support.constraint.ConstraintLayout;
 import android.widget.TextView;
 
 public class EX_3 extends AppCompatActivity {
 
-    private RelativeLayout mRelativeLayout;
+    private ConstraintLayout mRelativeLayout;
 
     private TextView mTextStr;
 
@@ -17,23 +17,22 @@ public class EX_3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex_3);
-        mRelativeLayout= (RelativeLayout) findViewById(R.id.relativeLayout);
-        //mTextStr = (TextView)findViewById(R.id.textStr);
+        mRelativeLayout= (ConstraintLayout) findViewById(R.id.relativeLayout);
+        mTextStr = (TextView)findViewById(R.id.textStr);
     }
-/*
+
     public void greenButtonClick(View view) {
         mTextStr.setText(R.string.green);
-       // mRelative.setBackgroundColor(ContextCompat.getColor(EX_3.this,R.color.greenColor));
+        mRelativeLayout.setBackgroundColor(ContextCompat.getColor(EX_3.this,R.color.greenColor));
     }
 
     public void yellowButtonClick(View view) {
         mTextStr.setText(R.string.yellow);
-       // mRelative.setBackgroundColor(ContextCompat.getColor(EX_3.this,R.color.yellowColor));
+        mRelativeLayout.setBackgroundColor(ContextCompat.getColor(EX_3.this,R.color.yellowColor));
     }
 
     public void redButtonClick(View view) {
         mTextStr.setText(R.string.red);
-       // mRelative.setBackgroundColor(ContextCompat.getColor(EX_3.this,R.color.redColor));
+        mRelativeLayout.setBackgroundColor(ContextCompat.getColor(EX_3.this,R.color.redColor));
     }
-*/
 }
